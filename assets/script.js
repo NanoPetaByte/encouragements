ba7e76eb062c4872c7780b7cc2b247f980ffdf9e
Zavier Chiam <60832719+NanoPetaByte@users.noreply.github.com>
2023-12-08 10:55:24 +0800

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -27,7 +27,8 @@ window.addEventListener("DOMContentLoaded", async () => {
 		override: [
 			document.getElementById("override-section"),
 			document.getElementById("override-list")
-		]
+		],
+		inviteCode: document.getElementById("login-code")
 	}
 	window.addEventListener("hashchange", (event) => {
 		event.hash = JSON.parse(atob(window.location.hash.split("#")[1]))
@@ -39,14 +40,10 @@ window.addEventListener("DOMContentLoaded", async () => {
 						if (tab[0].id === "success-tab") {
 							inputs.description.innerHTML = `Your message was successfully sent to <span style="color: rgba(255, 255, 255, 0.75)">${event.hash[2].name} - ${event.hash[2].lifegroup} (${event.hash[2].tribe} Tribe)</span>.`
 						} else if (tab[0].id === "messages-tab") {
-							if (!localStorage.getItem("cached-messages")) {
-								localStorage.setItem("cached-messages", JSON.stringify([]))
-							}
 							cached = JSON.parse(localStorage.getItem("cached-messages"))
 							inputs.messages.innerHTML = ""
 							cached.forEach((item) => {
 								item = [item, document.createElement("div")]
-								console.log(item[0])
 								item[1].className = "message"
 								item[1].innerHTML = `
 									<div class="profile">
@@ -70,35 +67,40 @@ window.addEventListener("DOMContentLoaded", async () => {
 		}
 	})
 	inputs.sendButton.onclick = function() {
-		(async () => {
-			request = await fetch("/messages/create", {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json"
-				},
-				body: JSON.stringify({
-					sender: profile.id,
-					content: inputs.editor.value,
-					receiver: inputs.autocomplete[0].userId
-				})
-			})
-			data = await request.json()
-			if (data.receiver.id === inputs.autocomplete[0].userId) {
-				cached.push({
-					receiver: {
-						id: data.receiver.id,
-						name: data.receiver.name,
-						tribe: data.receiver.tribe
+		if (!inputs.autocomplete[0].userId || inputs.editor.value.length < 1) {
+			console.log("failed.")
+		} else {
+			(async () => {
+				request = await fetch("/messages/create", {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json"
 					},
-					content: inputs.editor.value
+					body: JSON.stringify({
+						sender: profile.id,
+						content: inputs.editor.value,
+						receiver: inputs.autocomplete[0].userId
+					})
 				})
-				inputs.editor.value = ""
-				inputs.autocomplete[0].value = ""
-				inputs.autocomplete[0].userId = ""
-				localStorage.setItem("cached-messages", JSON.stringify(cached))
-				window.location.href = "#" + btoa(JSON.stringify(["tab", "success", data.receiver]))
-			}
-		})()
+				data = await request.json()
+				if (data.receiver.id === inputs.autocomplete[0].userId) {
+					cached.push({
+						receiver: {
+							id: data.receiver.id,
+							name: data.receiver.name,
+							tribe: data.receiver.tribe
+						},
+						content: inputs.editor.value
+					})
+					inputs.editor.value = ""
+					inputs.autocomplete[0].value = ""
+					inputs.autocomplete[0].userId = ""
+					localStorage.setItem("cached-messages", JSON.stringify(cached))
+					window.location.href = "#" + btoa(JSON.stringify(["tab", "success", data.receiver]))
+				}
+			})()
+		}
+		
 	}
 	inputs.loginButton.onclick = function() {
 		(async () => {
@@ -109,12 +111,12 @@ window.addEventListener("DOMContentLoaded", async () => {
 				},
 				body: JSON.stringify({
 					code: btoa(JSON.stringify({
-						email: inputs.email.value
+						email: inputs.email.value,
+						inviteCode: inputs.inviteCode.value
 					}))
 				})
 			})
 			profile = await request.json()
-			console.log(profile)
 			if (profile.length > 1) {
 				profile.forEach((account) => {
 					account = [account, document.createElement("button")]
@@ -128,6 +130,9 @@ window.addEventListener("DOMContentLoaded", async () => {
 							request = await fetch(`/assets/names.json?id=${profile.id}`)
 							names = await request.json()
 							if (names.length > 0) {
+								if (!localStorage.getItem("cached-messages") || (profile.messages < 1 && localStorage.getItem("cached-messages"))) {
+									localStorage.setItem("cached-messages", JSON.stringify([]))
+								}
 								window.location.href = "#" + btoa(JSON.stringify(["tab", "messages"]))
 							}
 						}
@@ -141,11 +146,16 @@ window.addEventListener("DOMContentLoaded", async () => {
 				names = await request.json()
 				if (names.length > 0) {
 					profile = profile[0]
+					if (!localStorage.getItem("cached-messages") || (profile.messages < 1 && localStorage.getItem("cached-messages"))) {
+						localStorage.setItem("cached-messages", JSON.stringify([]))
+					}
 					window.location.href = "#" + btoa(JSON.stringify(["tab", "messages"]))
 				} 
 			} else if (profile.error) {
 				inputs.email.style.backgroundColor = "#9c4040"
 				inputs.email.style.color = "rgba(255, 255, 255, 0.8)"
+				inputs.inviteCode.style.backgroundColor = "#9c4040"
+				inputs.inviteCode.style.color = "rgba(255, 255, 255, 0.8)"
 			}
 		})()
 	}
@@ -156,8 +166,13 @@ window.addEventListener("DOMContentLoaded", async () => {
 					if (!found.includes(name[3])) {
 						name = [name, document.createElement("button")]
 						name[1].id = name[0][3]
-						name[1].style.color = colors[name[0][2].toLowerCase()]
-						name[1].textContent = `${name[0][0]} - ${name[0][1]} (${name[0][2]} Tribe)`
+						if (colors[name[0][2].toLowerCase()]) {
+							name[1].style.color = colors[name[0][2].toLowerCase()]
+							name[1].textContent = `${name[0][0]} - ${name[0][1]} (${name[0][2]} Tribe)`
+						} else {
+							name[1].style.color = "#A24857"
+							name[1].textContent = `${name[0][0]} - ${name[0][1]}`
+						}
 						name[1].onclick = function() {
 							names.forEach((name) => {
 								if (name[3] === this.id) {
@@ -166,6 +181,9 @@ window.addEventListener("DOMContentLoaded", async () => {
 									inputs.autocomplete[0].style.backgroundColor = "#EEEEEE"
 									inputs.autocomplete[0].userId = this.id
 									inputs.autocomplete[1].style.display = "none"
+									if (!colors[name[2].toLowerCase()]) {
+										inputs.autocomplete[0].style.color = "#A24857"
+									}
 								}
 							})
 						}
@@ -175,6 +193,7 @@ window.addEventListener("DOMContentLoaded", async () => {
 				} else if (found.includes(name[3])) {
 					inputs.autocomplete[0].style.color = "rgba(255, 255, 255, 0.625)"
 					inputs.autocomplete[0].style.backgroundColor = "rgba(255, 255, 255, 0.0625)"
+					inputs.autocomplete[0].userId = undefined
 					inputs.autocomplete[1].removeChild(document.getElementById(name[3]))
 					found.splice(found.indexOf(name[3]), 1)
 				}
--- server.js
+++ server.js
@@ -12,15 +12,7 @@ const cacher = require("./cacher")
 const app = express()
 const server = http.createServer(app)
 
-const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-        user: "blyde33@gmail.com",
-        pass: "nrir lpyl wreb jweb"
-    }
-})
+const key = (fs.readFileSync("./storage/accounts.json").toString().length * (15 ** 14)).toString()
 
 const router = {
     assets: express.Router(),
@@ -32,11 +24,9 @@ const router = {
 let storage = {
     accounts: [],
     messages: [],
-    cached: []
+    cached: [],
+    released: [[], 0]
 }
-
-let total = 0
-let key = (fs.readFileSync("./storage/accounts.json").toString().length * (15 ** 14)).toString()
 
 app.use(cors())
 app.use(express.json())
@@ -50,14 +40,46 @@ app.get("/", (request, response) => {
     response.sendFile(__dirname + "/assets/index.html")
 })
 
-app.get("/reset", (request, response) => {
-    response.send(`
-        <script>
-            localStorage.removeItem("cached-messages")
-            window.location.href = window.location.origin
-        </script>
-    `)
-})
+async function release() {
+    template = fs.readFileSync("./assets/template.html").toString()
+    colors = { green: "#1F8B4C", black: "#000000", blue: "#206694", red: "#992D22" }
+    storage.accounts.forEach((account) => {
+        if (storage.messages[account[4]]) {
+            messages = []
+            storage.messages[account[4]].forEach((message) => {
+                sender = storage.accounts.filter((account) => account[4] === message[0])
+                messages.push(`
+                    <div class="message">
+                        <div class="profile">
+                            <div class="tribe" style="background-color: ${colors[sender[0][2].toLowerCase()]}"></div>
+                            <div class="info">
+                                <div class="name">${sender[0][0]}</div>
+                                <div class="id">${sender[0][4]}</div>
+                            </div>
+                        </div>
+                        <div class="content">${message[1]}</div>
+                    </div>
+                `)
+                storage.released[1] += 1
+            })
+            output = template
+                    .replace("{ messages }", messages.join("\n"))
+            /* transporter.sendMail({
+                from: config.user,
+                to: account[3],
+                subject: `${account[0]}'s YI Camp Encouragements`,
+                html: output
+            }, (error, info) => {
+                if (error) {
+                    console.log("Failed to send messages.", error)
+                } else {
+                    console.log(`Messages sent to ${account[0]}. [${(output.length / (1024 * 1024)).toFixed(2)} MB]`)
+                }
+            }) */
+            console.log(`Messages sent to ${account[0]}. [${(output.length / (1024 * 1024)).toFixed(2)} MB]`)
+        }
+    })
+}
 
 router.assets.get("/:id", (request, response) => {
     if (fs.existsSync(`./assets/${request.params.id}`)) {
@@ -65,7 +87,6 @@ router.assets.get("/:id", (request, response) => {
             response.sendFile(__dirname + `/assets/${request.params.id}`)
         }
     } else if (request.params.id.includes("names.json")) {
-        /*
         storage.accounts.forEach((account) => {
             if (account[4] === request.query.id) {
                 found = storage.accounts.filter((account) => account[4] !== request.query.id)
@@ -73,8 +94,6 @@ router.assets.get("/:id", (request, response) => {
             }
         })
         response.json(found)
-        */
-        response.json(storage.accounts.map((account) => ([account[0], account[1], account[2], account[4]])))
     }
 })
 
@@ -85,12 +104,20 @@ router.accounts.post("/authorize", (request, response) => {
     if (found.length < 1) {
         response.json({ error: "Failed to Authorize." })
     } else {
-        response.json(found.map(account => ({ 
+        found = found.filter((account) => (
+            btoa(account[2] + account[1]).slice(-10, -2) === data.inviteCode
+        )).map(account => ({ 
             id: account[4], 
             name: account[0], 
             lifegroup: account[1],
-            tribe: account[2]
-        })))
+            tribe: account[2],
+            messages: (storage.cached.filter((message) => message[0][0] === account[4])).length
+        }))
+        if (found.length > 0) {
+            response.json(found)
+        } else {
+            response.json({ error: "Failed to Authorize." })
+        }
     }
 })
 
@@ -109,6 +136,9 @@ router.messages.post("/create", (request, response) => {
             request.body.sender,
             request.body.content.split("\n").join("<br>")
         ])
+        storage.cached.push([
+            [request.body.sender, found[4]], request.body.content.split("\n").join("<br>")
+        ])
         response.json({
             receiver: {
                 id: found[4],
@@ -117,63 +147,67 @@ router.messages.post("/create", (request, response) => {
                 tribe: found[2]
             }
         })
-        total += 1
-        console.log(`Total Messages: ${total} [${(fs.statSync("./storage/messages.json").size/(1024 * 1024)).toFixed(2)} MB]`)
         fs.writeFileSync("./storage/messages.json", JSON.stringify(storage.messages))
     }
 })
 
-router.admin.get("/release/:id", (request, response) => {
-    messages = []
-    template = fs.readFileSync("./assets/template.html").toString()
-    colors = { green: "#1F8B4C", black: "#000000", blue: "#206694", red: "#992D22" }
-    account = storage.accounts.filter((account) => account[4] === request.params.id)
-    if (account.length > 0 || storage.messages[request.params.id]) {
-        storage.messages[account[0][4]].forEach((message) => {
-            sender = storage.accounts.filter((account) => account[4] === message[0])
-            messages.push(`
-                <div class="message">
-                    <div class="profile">
-                        <div class="tribe" style="background-color: ${colors[sender[0][2].toLowerCase()]}"></div>
-                        <div class="info">
-                            <div class="name">${sender[0][0]}</div>
-                            <div class="id">${sender[0][4]}</div>
-                        </div>
-                    </div>
-                    <div class="content">${message[1]}</div>
-                </div>
-            `)
+router.admin.post("/:action", (request, response) => {
+    if (request.params.action === "statistics") {
+        response.json({
+            messages: {
+                size: [
+                    (JSON.stringify(storage.messages).length / (1024)),
+                    ((JSON.stringify(storage.messages).length/(1024**2))*100).toFixed(1) + "%"
+                ],
+                total: storage.cached.length
+            },
+            accounts: {
+                size: (JSON.stringify(storage.accounts).length / (1024)),
+                total: storage.accounts.length
+            },
+            released: storage.released[1]
         })
-        output = template
-                    .replace("{ messages }", messages.join("\n"))
-                    .replace("{ banner url }", "data:image/jpg;base64," + fs.readFileSync("./assets/banner.jpg").toString("base64"))
-        response.send(output)
-        /*
-        transporter.sendMail({
-            from: "blyde33@gmail.com",
-            to: account[0][3],
-            subject: "YI Camp Encouragements",
-            html: output
-        }, (error, info) => {
-            if (error) {
-                response.send("Failed to send messages.")
-            } else {
-                response.send(`Messages sent to ${account[0][3]}. [${(output.length / (1024 * 1024)).toFixed(2)} MB]`)
-            }
-        })
-        */
+    } else if (request.params.action === "release") {
+        if (storage.released[1] < 1) {
+            release()
+        } else {
+            response.json({ status: "has already started." })
+        }
     }
 })
 
 server.listen(5000, () => {
-    /* fs.writeFileSync("./storage/accounts.json", cacher.encrypt(fs.readFileSync("./storage/accounts.json").toString(), key)) */
     if (!fs.existsSync("./storage/messages.json")) {
         fs.writeFileSync("./storage/messages.json", JSON.stringify({}))
     }
     storage = {
         accounts: JSON.parse(cacher.decrypt(fs.readFileSync("./storage/accounts.json").toString(), key)),
         messages: JSON.parse(fs.readFileSync("./storage/messages.json")),
-        cached: []
+        cached: [], released: [[], 0]
     }
+    config = JSON.parse(cacher.decrypt(fs.readFileSync("./config.json").toString(), key))
+    transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+            user: config.user,
+            pass: config.pass
+        }
+    })
+    lifegroups = []
+    storage.accounts.forEach((account) => {
+        if (storage.messages[account[4]]) {
+            storage.messages[account[4]].forEach((message) => {
+                storage.cached.push([
+                    [message[0], account[4]], message[1]
+                ])
+            })
+        }
+        if (!lifegroups.includes(account[1] + account[2])) {
+            lifegroups.push(account[1] + account[2])
+            console.log("**", account[1], "**", "`", btoa(account[2] + account[1]).slice(-10, -2), "`")
+        }
+    })
     console.log("Server Online.")
 })
